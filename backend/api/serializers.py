deb3d929458688ee6c3c5a2b387da6da63dd4e69
Lusine <lusine@getsteven.com>
2024-01-26 01:19:02 +0400

    Add serializers for favorite and shopping list.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from recipes.models import Tag, Ingredient, Recipe
+from recipes.models import (
+    Tag, Ingredient, Recipe, FavoriteRecipe, ShoppingList)
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,3 +20,37 @@ class RecipeSummarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('user', 'recipe')
+        model = FavoriteRecipe
+
+    def validate(self, data):
+        current_user = data.get('user')
+        if current_user.favorites.filter(recipe=data.get('recipe')).exists():
+            raise serializers.ValidationError(
+                'This recipe is already in Favorites.')
+        return data
+
+    def to_representation(self, instance):
+        recipe = instance.recipe
+        return RecipeSummarySerializer(recipe).data
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('user', 'recipe')
+        model = ShoppingList
+
+    def validate(self, data):
+        current_user = data.get('user')
+        if current_user.favorites.filter(recipe=data.get('recipe')).exists():
+            raise serializers.ValidationError(
+                'This recipe is already in Shopping List.')
+        return data
+
+    def to_representation(self, instance):
+        recipe = instance.recipe
+        return RecipeSummarySerializer(recipe).data
